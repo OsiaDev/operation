ffb7a943b68a908ecda4ef66106db32092e77c52
Cristian Osia <osia.developer@gmail.com>
2025-11-10 07:53:35 -0500

    Init

---
+++ src/test/java/co/cetad/umas/operation/UmasOperationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.cetad.umas.operation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmasOperationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
